ab3dc48bf0fbfd639ff1b62971af0dae25a3527d
Wassup <nyeiamara1@gmail.com>
2022-04-08 11:30:58 +0000

    My puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void)
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(v0id)
 {
-puts("\"programming is building a multilingual puzzle");
+puts("\"programming is like buliding a multilingual puzzle");
 return (0);
 }

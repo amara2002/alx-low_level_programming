567650dde4f73360c1f5131fd5637fed8d2bfcb6
Wassup <nyeiamara1@gmail.com>
2022-04-09 15:00:20 +0000

    My hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  *main - entry point
  *My hello, printf task
+ Return: return 1 after program execution
  *Return: return 1 after program execution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

f5640132a85c5bdc21f503f396245473d38dd40e
Wassup <nyeiamara1@gmail.com>
2022-04-09 14:26:52 +0000

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
+#!/bin/bash
 #include <stdio.h>
-/**
-*main - entry point
-*
-*Return: 0 after printing the function
-*/
-int main(v0id)
+int main(void)
 {
-puts("\"programming is like buliding a multilingual puzzle");
+puts("\"programming is like a building a multilingual puzzle");
 return (0);
 }

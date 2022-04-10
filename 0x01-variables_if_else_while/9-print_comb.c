0f54726a1701df3bf31a723fd6c44cc7b94de2d4
Wassup <nyeiamara1@gmail.com>
2022-04-10 13:39:38 +0000

    My message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int x;
+
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+if (x == 57)
+{
+break;
+}
+putchar(',');
+putchar (' ');
+}
+putchar('\n');
+return (0);
+}

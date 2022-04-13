3881aaacf2a3699fb703cf9c948b5b3fea9c9370
Wassup <nyeiamara1@gmail.com>
2022-04-13 11:47:58 +0000

    My file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * Return: Always 0 (success/correct)
+ *main-main block
+ *Description print _putchar in a new line
+ *Return: this program returns zero
  */
 int main(void)
 {
-char ermias[] = "_putchar";
-int c;
-
-for (c = 0; c < 8; c++)
-{
-   _putchar(ermias[c]);
-}
- _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }

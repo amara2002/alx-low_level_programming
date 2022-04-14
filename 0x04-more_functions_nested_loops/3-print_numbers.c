da19acd8d0dcefeceb459d4ad0c42f817fc1c061
Wassup <nyeiamara1@gmail.com>
2022-04-14 13:43:20 +0000

    mY MO

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - print numbers
@@ -11,10 +12,10 @@ int i;
 i = 0;
 while (i < 10)
 {
-_putchar (i + '0');
+ _putchar (i + '0');
 
 i++;
 }
-_putchar ('\n');
+ _putchar ('\n');
 return (0);
 }

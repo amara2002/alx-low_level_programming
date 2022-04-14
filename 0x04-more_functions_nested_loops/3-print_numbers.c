494791820b36ebdc569e8f2c66011c41a6a1d17a
Wassup <nyeiamara1@gmail.com>
2022-04-14 13:50:09 +0000

    My mme

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - print numbers
@@ -8,14 +7,14 @@
 void print_numbers(void)
 {
 int i;
-
 i = 0;
+
 while (i < 10)
 {
- _putchar (i + '0');
-
+_putchar (i + '0');
 i++;
 }
  _putchar ('\n');
-return (0);
+
 }
+

ca276295c874f6ccd2c606966dc68b22a0cf5794
Wassup <nyeiamara1@gmail.com>
2022-04-28 13:27:09 +0000

    My second task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-int x;
-x = 0;
+/*Base condition*/
 if (*s == '\0')
 return (0);
-x = 1 + _strlen_recursion(s + 1);
-return (x);
+else
+return (1 + _strlen_recursion(s + 1): /*sum*/
 }

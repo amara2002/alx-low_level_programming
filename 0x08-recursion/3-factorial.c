16cb12adb298659bb636f26e65ccfedb6130a8de
Wassup <nyeiamara1@gmail.com>
2022-04-28 13:40:21 +0000

    My third task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *factorial - factorial of a given number
- *@n: pointer block of memory to fill
- *Return: factorial
+ * factorial - factorial of a given number
+ * @n: pointer block of memory to fill
+ *
+ * Return: factorial
  */
 int factorial(int n)
 {

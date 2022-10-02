7f69aeda03fc239cad9d56f070f4d2a7a5e6b4e2
amara2002 <nyeiamara1@gmail.com>
2022-10-03 00:19:41 +0200

    My factorial task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *factorial - factorial of a given number
- *@n: pointer block of memory to fill
+ *factorial - returns factorial of a given number.
+ *@n: number to find factorial of.
  *
- *Return: factorial
+ *Return: factorial of number
+ *-1 if n < 0
  */
 
 int factorial(int n)
 {
-       if (n == 0) /*Base condition*/
-                return(1);
-       else if (n < 0) /*Base condition*/
-                return(-1);
-       else
-  return (n * factorial(n - 1)); /*Recursive call*/
+       if (n < 0)
+	       return (-1);
+       if (n == 0 || n == 1)
+	       return (1);
+       return (n * factorial(n-1));
 }

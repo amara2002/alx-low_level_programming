795c0d21da4e792005437e44bc51c902152da1b8
amara2002 <nyeiamara1@gmail.com>
2022-10-03 00:08:27 +0200

    My factorial task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,7 +12,7 @@ int factorial(int n)
 if (n == 0) /*Base condition*/
 return(1);
 else if (n < 0) /*Base condition*/
- return(-1);
+return(-1);
 else
 return (n * factorial(n - 1)); /*Recursive call*/
 }

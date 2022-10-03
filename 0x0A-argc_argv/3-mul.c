351237715bb1f3434882efa9e72d7f3d229fc522
amara2002 <nyeiamara1@gmail.com>
2022-10-03 02:37:51 +0200

    My third task of mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int index, multiplication;
-	multiplication = 1;
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	for (index = 1; index < argc; index++)
-	{
-		multiplication = multiplication * atoi(argv[index]);
-	}
-	printf("%d\n", multiplication);
-	return (0);
+int index, multiplication;
+multiplication = 1;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+for (index = 1; index < argc; index++)
+{
+multiplication = multiplication * atoi(argv[index]);
+}
+printf("%d\n", multiplication);
+return (0);
 }
